d8a1557d3abc291695e544099850854a3d5953ee
Matthew Listor <mattlistor@gmail.com>
2019-07-12 03:54:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,10 @@ def nyc_pigeon_organizer (data)
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
+        if result[name] == false
+        else
         result[name] = {:color => [], :gender => [], :lives => []}
+        end
       end 
     end 
   end 
