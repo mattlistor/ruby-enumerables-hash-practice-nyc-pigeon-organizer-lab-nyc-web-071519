e34865bf0c69897c30e78f3545821fc26a6496ed
Matthew Listor <mattlistor@gmail.com>
2019-07-12 04:11:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,33 +16,39 @@ def nyc_pigeon_organizer (data)
   color = data[:color]
   color.each do |birdColor, name|
     name.each do |birdName|
+      
       pigeonNames.each do |item|
         if birdName === item
           result[item][:color].push(birdColor.to_s)
         end 
       end 
+      
     end 
   end 
   
   gender = data[:gender]
   gender.each do |gender, name|
     name.each do |birdName|
+      
       pigeonNames.each do |item|
         if birdName === item
           result[item][:gender].push(gender.to_s)
         end 
       end 
+      
     end 
   end
   
   lives = data[:lives]
   lives.each do |location, name|
     name.each do |birdName|
+      
       pigeonNames.each do |item|
         if birdName === item
           result[item][:lives].push(location.to_s)
         end 
       end 
+      
     end 
   end 
   
